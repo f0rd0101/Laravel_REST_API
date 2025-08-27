b4bc0e77585769868fa71febfcc322b002aca692
Nick <melancholic8078@gmail.com>
2025-08-28 02:04:05 +0300

    comment erased

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,25 +11,7 @@ class AuthController extends Controller
     public function __construct(AuthService $authService){
        $this->authService = $authService;
     }
-//    public function register(Request $request){
-//     $validatedBody = $request->validate([
-//         'name' => ['string','required','max:50'],
-//         'email' => ['string','required','max:30','unique:users,email'],
-//         'password' => ['string','required','max:20','min:8'],
-//     ],[
-//         'name.required' => 'Name is required',
-//         'email.required'=> 'Email is required',
-//         'password.required'=> 'Password is required',
-//         'email.unique' => 'This email is already in use',
-//         'password.min'=> 'Password must be at least 8 characters',
-//     ]);
 
-//     $res = $this->authService->registerUser($validatedBody);
-
-//     return response()->json($res,201);
-
-    
-// }
 
 public function register(Request $request){
     try {
